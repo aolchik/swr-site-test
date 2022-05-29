98ce6fb20c2abd0f1e1bb0e86c4eef770e6b75f3
Alejandro <alejandro@orulo.com.br>
2022-05-29 09:00:23 -0300

    feat: add first netlify content

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,26 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Netlify Widget */}
+          <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+        </Head>
         <body>
           <SkipNavLink />
           <Main />
           <NextScript />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", user => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
+                });
+              }
+          `}}/>
         </body>
       </Html>
     )
