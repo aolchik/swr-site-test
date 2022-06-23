c557de5f81d260d1b17712eb3bb6db3813eab62f
Alejandro <alejandro@orulo.com.br>
2022-06-23 05:36:13 -0300

    fix: add missing pt-BR content

--- components/features.tsx
+++ components/features.tsx
@@ -17,12 +17,8 @@ const Feature = ({ text, icon }: FeatureProps) => (
 
 const TITLE_WITH_TRANSLATIONS = {
   "en-US": "React Hooks for Data Fetching",
-  "es-ES": "Biblioteca React Hooks para la obtención de datos",
-  "zh-CN": "用于数据请求的 React Hooks 库",
-  ja: "データ取得のための React Hooks ライブラリ",
-  ko: "데이터 가져오기를 위한 React Hooks",
-  ru: "React хуки для выборки данных",
-};
+  "pt-BR": "React Hooks para Data Fetching",
+}
 
 // Transaltions for Features
 const FEATURES_WITH_TRANSLATIONS = {
@@ -36,28 +32,15 @@ const FEATURES_WITH_TRANSLATIONS = {
     typescript: "TypeScript Ready",
     remoteLocal: "Remote + Local",
   },
-  "es-ES": {},
-  "zh-CN": {
-    lightweight: "Lightweight",
-    realtime: "Realtime",
+  "pt-BR": {
+    lightweight: "Leve",
+    realtime: "Tempo-real",
     suspense: "Suspense",
-    pagination: "Pagination",
-    backendAgnostic: "Backend Agnostic",
-    renderingStrategies: "SSR / SSG Ready",
-    typescript: "TypeScript Ready",
-    remoteLocal: "Remote + Local",
-  },
-  ja: {},
-  ko: {},
-  ru: {
-    lightweight: "Лёгкий",
-    realtime: "В реальном времени",
-    suspense: "Задержка",
-    pagination: "Пагинация",
-    backendAgnostic: "Бэкэнд-независимый",
-    renderingStrategies: "SSR / SSG",
-    typescript: "TypeScript",
-    remoteLocal: "Удалённо + Локально",
+    pagination: "Paginação",
+    backendAgnostic: "Backend Agnóstico",
+    renderingStrategies: "Pronto para SSR / SSG",
+    typescript: "Pronto para TypeScript",
+    remoteLocal: "Remoto + Local",
   },
 };
 
